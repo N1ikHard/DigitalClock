bd55c446a6264222ebbdb69c75d155951615bef8
N1ikHard <N1ik@yandex.ru>
2021-05-21 15:02:16 +0300

    Добавьте файлы проекта.

---
+++ DigitalClock/Digit.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalClock
+{
+    /// <summary>
+    /// Логика взаимодействия для Digit.xaml
+    /// </summary>
+    public partial class Digit : UserControl
+    {
+        public Digit()
+        {
+            InitializeComponent();
+        }
+        public Digit(int num) : this()
+        {
+            SetDigit(num);
+        }
+        public void SetDigit(int num)
+        {
+            SetOff();
+            switch (num) 
+            {
+                case 0:
+                    SetOn(A);
+                    SetOn(B);
+                    SetOn(C);
+                    SetOn(D);
+                    SetOn(E);
+                    SetOn(F);
+                    break;
+                case 1:
+                    SetOn(A);
+                    SetOn(B);
+                    break;
+                case 8:
+                    SetOn(A);
+                    SetOn(B);
+                    SetOn(C);
+                    SetOn(D);
+                    SetOn(E);
+                    SetOn(F);
+                    SetOn(G);
+                    break;
+                case 2:
+                    SetOn(A);
+                 
+                    SetOn(C);
+                    SetOn(D);
+                    SetOn(F);
+                    SetOn(G);
+                    break;
+                case 3:
+                    SetOn(A);
+                    SetOn(B);
+                    SetOn(C);
+                    SetOn(G);
+                    SetOn(F);
+                    break;
+                case 4:
+                    SetOn(A);
+                    SetOn(B);
+                    SetOn(E);
+                    SetOn(G);
+                    break;
+                case 5:
+                    SetOn(B);
+                    SetOn(C);
+                    SetOn(G);
+                    SetOn(E);
+                    SetOn(F);
+                    break;
+                case 6:
+                    SetOn(B);
+                    SetOn(C);
+                    SetOn(D);
+                    SetOn(E);
+                    SetOn(G);
+                    SetOn(F);
+                    break;
+                case 7:
+                    SetOn(A);
+                    SetOn(B);
+                    SetOn(F);
+                    break;
+                case 9:
+                    SetOn(A);
+                    SetOn(B);
+                    SetOn(E);
+                    SetOn(F);
+                    break;
+
+            }
+        }
+        private void SetOff()
+        {
+            A.Fill = new SolidColorBrush(Colors.Transparent);
+            B.Fill = new SolidColorBrush(Colors.Transparent);
+            C.Fill = new SolidColorBrush(Colors.Transparent);
+            D.Fill = new SolidColorBrush(Colors.Transparent);
+            E.Fill = new SolidColorBrush(Colors.Transparent);
+            F.Fill = new SolidColorBrush(Colors.Transparent);
+            G.Fill = new SolidColorBrush(Colors.Transparent);
+        }
+        private void SetOn(Path A)
+        {
+            Brush brush = new SolidColorBrush(Colors.Red);
+            A.Fill = brush;
+        }
+    }
+}
---
+++ DigitalClock/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows;
+namespace DigitalClock
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        Digit[] digit = new Digit[4];
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            for(int i = 0; i < digit.Length; i++)
+            {
+                 digit[i] = new Digit(8);
+                digit[i].SetValue(Grid.ColumnProperty, i < 2 ? i : i + 1);
+                digit[i].SetValue(Grid.RowSpanProperty, 2);
+                MainGrid.Children.Add(digit[i]);
+            }
+        }
+      
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now.Second % 2 == 0)
+            {
+                PointA.Fill = new SolidColorBrush(Colors.Transparent);
+                PointB.Fill = new SolidColorBrush(Colors.Transparent);
+            }
+            else
+            {
+                PointA.Fill = new SolidColorBrush(Colors.Red);
+                PointB.Fill = new SolidColorBrush(Colors.Red);
+            }
+            digit[0].SetDigit(DateTime.Now.Hour / 10);
+            digit[1].SetDigit(DateTime.Now.Hour % 10);
+            digit[2].SetDigit(DateTime.Now.Minute / 10);
+            digit[3].SetDigit(DateTime.Now.Minute%10);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
